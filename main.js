095f5b496c4cebcc090c25e6d609896bbdf18783
someone <someone@someplace.com>
2021-01-19 15:44:48 +0800

    some init msg

---
+++ main.js
@@ -0,0 +1,22 @@
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+function openMenu(evt, menuName) {
+  var i, x, tablinks;
+  x = document.getElementsByClassName("menu");
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < x.length; i++) {
+     tablinks[i].className = tablinks[i].className.replace("");
+  }
+  document.getElementById(menuName).style.display = "block";
+  
+}
